d60187b6647508510e32ac4d5fddf611953b7750
Darshan Satya <darshans1989@gmail.com>
2021-05-27 21:21:47 -0400

    mocking

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -1,11 +1,17 @@
-import { screen, render } from "@testing-library/react"
-import Asyncs from "./Async"
+import { screen, render } from "@testing-library/react";
+import Asyncs from "./Async";
 
-describe('Async Component', () => {
-    test("renders post if request succeeds", async () => {
-        render(<Asyncs />)
+describe("Async Component", () => {
+  test("renders post if request succeeds", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 1, title: "firstpost" }],
+    });
+    render(<Asyncs />);
 
-        const listItemElements = await screen.findAllByRole('listitem');
-        expect(listItemElements).not.toHaveLength(0);
-    })
-})
+    const listItemElements = await screen.findAllByRole("listitem");
+    const firstPostElement = screen.getByText("firstpost");
+    expect(listItemElements).not.toHaveLength(0);
+    expect(firstPostElement).toBeInTheDocument();
+  });
+});
